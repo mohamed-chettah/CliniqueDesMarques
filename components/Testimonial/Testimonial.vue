ebfdda26f6f99a35b6322067ad8dea7bb5f65b71
mohamedDev69 <mohamedchettah0208@gmail.com>
2025-01-01 22:24:51 +0100

    wip testimonial + hover button

--- components/Testimonial/Testimonial.vue
+++ components/Testimonial/Testimonial.vue
@@ -4,7 +4,8 @@ const testimonials = shallowRef([
     {
       name: "Emily Johnson",
       username: "@emilyj",
-      text: "This service transformed my business. The tools provided are easy to use and incredibly effective. Highly recommend!",
+      text: "This service transformed my business. The tools" +
+          "\n  provided are easy to use and incredibly effective. Highly recommend!",
     },
     //Add more data
   ],
@@ -12,7 +13,8 @@ const testimonials = shallowRef([
     {
       name: "Laura Fernández",
       username: "@laurafern",
-      text: "La mejor herramienta que he usado para crear landing pages. Es rápida, confiable, y el equipo de soporte es muy útil.",
+      text: "La mejor herramienta que he usado para crear " +
+          "\n landing pages. Es rápida, confiable, y el equipo de soporte es muy útil.",
     },
     //Add more data
   ],
@@ -20,7 +22,8 @@ const testimonials = shallowRef([
     {
       name: "David Wilson",
       username: "@davidw",
-      text: "Exceeded all my expectations! The UI is beautiful and the features are exactly what I needed for my project.",
+      text: "Exceeded all my expectations! The UI is beautiful" +
+          "\n  and the features are exactly what I needed for my project.",
     },
     //Add more data
   ],
@@ -28,7 +31,8 @@ const testimonials = shallowRef([
     {
       name: "Sophie Li",
       username: "@sophieli",
-      text: "I've been using this service for a few months now and I'm very happy with the results. It's easy to use and the support team is great.",
+      text: "I've been using this service for a few months now and I'm very happy with " +
+          "\n the results. It's easy to use and the support team is great.",
     },
     //Add more data
   ],
@@ -36,7 +40,8 @@ const testimonials = shallowRef([
     {
       name: "John Smith",
       username: "@johnsmith",
-      text: "I've tried a few different services and this one is by far the best. The features are great and the support is top-notch.",
+      text: "I've tried a few different services and this one is by far the best. " +
+          "\n The features are great and the support is top-notch.",
     },
     //Add more data
   ],
@@ -44,7 +49,8 @@ const testimonials = shallowRef([
     {
       name: "Maria García",
       username: "@mariag",
-      text: "Me encanta esta herramienta. Es fácil de usar y tiene todas las funciones que necesito para mis proyectos. ¡Muy recomendable!",
+      text: "Me encanta esta herramienta. " +
+          "\n Es fácil de usar y tiene todas las funciones que necesito para mis proyectos. ¡Muy recomendable!",
     },
     //Add more data
 ],
@@ -53,12 +59,17 @@ const testimonials = shallowRef([
 
 <template>
 
+  <NuxtMarquee
+      direction="up"
+      gradientColor="var(--color-dark-50)"
+  >
+  </NuxtMarquee>
+
     <div class="xl:mx-0 mx-10 mt-10">
 
       <div class="border mt-10 p-10 bg-[url('/images/abstract-purple.jpeg')]  bg-cover bg-no-repeat rounded-xl">
 
         <div class="flex my-16 rounded-2xl mx-auto">
-          <div class=" bg-white blur-lg text-transparent ">ll</div>
           <NuxtMarquee
               direction="right"
               gradientColor="var(--color-dark-50)"
@@ -68,14 +79,14 @@ const testimonials = shallowRef([
               <template v-for="(col, index) in testimonials" :key="index">
 
                 <div
-                    class="col-span-full flex h-full flex-col gap-4 even:mt-4 md:col-span-4"
+                    class="col-span-full flex h-full flex-col gap-2 even:mt-4 md:col-span-4"
                 >
                   <template v-for="(testimonial, _index) in col" :key="_index">
                     <div
-                        class="border-dark-200 bg-dark-100 hover:bg-dark-200/70 dark:bg-dark-800/50 dark:border-dark-800 dark:hover:bg-dark-800/70 rounded-2xl border p-4 transition-all duration-300 ease-in-out"
+                        class=" bg-beige border rounded-xl p-8 transition-all duration-300 ease-in-out"
                     >
                       <p
-                          class="text-dark-950/80 dark:text-dark-50/60 text-base font-normal"
+                          class="text-dark-950/80 dark:text-dark-50/60 text-base whitespace-pre-line font-normal"
                       >
                         {{ testimonial.text }}
                       </p>
