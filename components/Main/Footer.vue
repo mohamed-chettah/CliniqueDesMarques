50670493a34c1c90732a9d13f2405bac2ccca628
mohamedDev69 <mohamedchettah0208@gmail.com>
2025-01-01 16:20:15 +0100

    change footer color

--- components/Main/Footer.vue
+++ components/Main/Footer.vue
@@ -92,14 +92,14 @@ const product_navigation = [
 
 <template>
   <footer
-      class="flex flex-col p-10 mt-20 bg-[url('/images/image-mesh-gradient-3.png')] bg-cover bg-no-repeat text-white"
+      class="text-white flex flex-col p-10 mt-20 bg-[url('/images/image-mesh-gradient-3.png')] bg-cover bg-no-repeat text-white"
   >
-    <div class="mx-auto container relative z-10 grid w-full grid-cols-12 gap-y-4 lg:gap-y-0">
+    <div class="mx-auto container relative z-10 grid w-full grid-cols-10 gap-y-4 lg:gap-y-0">
       <div
           class="col-span-full flex flex-col items-start justify-start gap-y-6 lg:col-span-4 xl:col-span-8"
       >
         <span
-            class="text-dark-950 dark:text-dark-50/50 absans inline-block w-full text-lg font-medium md:w-1/2"
+            class="text-white absans inline-block w-full text-lg font-medium md:w-1/2"
         >
           La Clinique des marques
         </span>
@@ -135,7 +135,7 @@ const product_navigation = [
       >
         <template v-for="(nav, index) in navigation" :key="index">
           <li
-              class="text-dark-950 dark:text-dark-50 font-inter mb-3 text-base font-medium capitalize"
+              class="text-white font-inter mb-3 text-base font-medium capitalize"
           >
             {{ nav.label }}
           </li>
@@ -158,7 +158,7 @@ const product_navigation = [
       >
         <template v-for="(nav, index) in product_navigation" :key="index">
           <li
-              class="text-dark-950 dark:text-dark-50 font-inter mb-3 text-base font-medium capitalize"
+              class="text-white font-inter mb-3 text-base font-medium capitalize"
           >
             {{ nav.label }}
           </li>
@@ -167,32 +167,8 @@ const product_navigation = [
                 :to="inner_link.link"
                 target="_blank"
                 variant="link"
-                color="neutral"
                 size="lg"
-                class="p-0 hover:underline"
-            >
-              {{ inner_link.label }}
-            </UButton>
-          </li>
-        </template>
-      </ul>
-      <ul
-          class="col-span-full flex w-full flex-col gap-y-2 md:col-span-6 lg:col-span-2 xl:col-span-1"
-      >
-        <template v-for="(nav, index) in other_navigation" :key="index">
-          <li
-              class="text-dark-950 dark:text-dark-50 font-inter mb-3 text-base font-medium capitalize"
-          >
-            {{ nav.label }}
-          </li>
-          <li v-for="(inner_link, _index) in nav.links" :key="_index">
-            <UButton
-                :to="inner_link.link"
-                target="_blank"
-                variant="link"
-                color="neutral"
-                size="lg"
-                class="p-0 hover:underline"
+                class="text-white p-0 hover:underline"
             >
               {{ inner_link.label }}
             </UButton>
@@ -200,7 +176,7 @@ const product_navigation = [
         </template>
       </ul>
       <div
-          class="gap-1 text-black border-dark-950/10 dark:border-dark-50/10 text-dark-950/50 dark:text-dark-50/50 col-span-full mt-4 mb-2 flex items-center justify-center border-t pt-4 text-sm"
+          class="gap-1 text-white border-dark-950/10 dark:border-dark-50/10 text-dark-950/50 dark:text-dark-50/50 col-span-full mt-4 mb-2 flex items-center justify-center border-t pt-4 text-sm"
       >
        <p>© Made with ❤️ by </p>  <a class="text-[#F0BF6C]" href="https://www.mc-studio.eu/"> MC Studio</a>
       </div>
